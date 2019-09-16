1ed0997c6a41e0f84837e48a14f7f0ac0881fbb5
Nick James <thenickjames@gmail.com>
2019-09-16 12:56:27 +0700

    Part 1: Finished deployement stuff

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,18 +1,23 @@
+import os
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SECRET_KEY = 'my_precious'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://postgres:postgres@127.0.0.1:5435/users_dev')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL', 'postgres://postgres:postgres@127.0.0.1:5435/users_test')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
---
+++ services/users/project/tests/test_users.py
@@ -0,0 +1,139 @@
+import json
+import unittest
+
+from project import db
+from project.tests.base import BaseTestCase
+from project.api.models import User
+
+
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+class TestUserService(BaseTestCase):
+    """Tests for the Users Service."""
+
+    def test_users(self):
+        """Ensure the /ping route behaves correctly."""
+        response = self.client.get('/users/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'nick',
+                    'email': 'nickjames@blah.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('nickjames@blah.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_add_user_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys(self):
+        """
+        Ensure error is thrown if the JSON object does not have a username key.
+        """
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({'email': 'nickjames@blah.com'}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_email(self):
+        """Ensure error is thrown if the email already exists."""
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'nick',
+                    'email': 'nickjames@blah.com'
+                }),
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'nick',
+                    'email': 'nickjames@blah.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn(
+                'Sorry. That email already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+    
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = add_user('nick','nickjames@blah.com')
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assert200(response)
+            self.assertEqual('nick', data['data']['username'])
+            self.assertIn('nickjames@blah.com', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/stuff')
+            data = json.loads(response.data.decode())
+            self.assert404(response)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/123')
+            data = json.loads(response.data.decode())
+            self.assert404(response)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_all_users(self):
+        add_user('nick', 'nick@james.com')
+        add_user('josh', 'josh@daniels.com')
+
+        with self.client:
+            response = self.client.get('/users')
+            data = json.loads(response.data.decode())
+            self.assert200(response)
+            self.assertEqual(2, len(data['data']['users']))
+            self.assertEqual('nick', data['data']['users'][0]['username'])
+            self.assertEqual('nick@james.com', data['data']['users'][0]['email'])
+            self.assertEqual('josh', data['data']['users'][1]['username'])
+            self.assertEqual('josh@daniels.com', data['data']['users'][1]['email'])
+            self.assertIn('success', data['status'])
+
+if __name__ == '__main__':
+    unittest.main()
